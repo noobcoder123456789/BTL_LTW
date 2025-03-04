a5fbd24baee4f1984aa9eaba39b435ff7a0722f9
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-03-04 08:22:25 +0700

    update phan2_bai2 lab02

--- Lab/Lab02/phan2_bai2/preprocess.js
+++ Lab/Lab02/phan2_bai2/preprocess.js
@@ -1,17 +1,101 @@
+class stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(ele) {
+        this.stack.push(ele);
+    }
+
+    pop() {
+        this.stack.pop();
+    }
+
+    size() {
+        return this.stack.length;
+    }
+
+    empty() {
+        return this.size() == 0;
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+}
+
+const displayExpression = document.querySelector('#expression');
+const displayCurrentValue = document.querySelector('#curVal');
 const buttons = document.querySelectorAll('.btn');
+
+let currExpr = "";
+let currVal = "";
+let st = new stack();
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        function isOperation(op) {
+            return op === '+' || op === '-' || op === 'x' || op === '/' || op === '%' || op === '^';
+        }
+
         const value = button.textContent.trim();
 
-        fetch('logic.php', {
-            method: 'POST', 
-            headers: {
-                'Content-type': 'application/json'
-            }, 
-            body: JSON.stringify({
-                value: value
-            })
-        });
+        if(value === 'AC') {
+            currExpr = currVal = "";
+            st = new stack();
+            displayExpression.value = '';
+            displayCurrentValue.value = '0';
+            return;
+        }
+
+        if(value === '=') {
+            let isOp = false;
+            for(; !st.empty(); st.pop()) {
+                let temp = st.top(); 
+                
+                if(isOperation(temp)) {
+                    isOp = true;
+                }
+            }
+
+            return;
+        }        
+        
+        if(isOperation(value)) {
+            if(st.empty()) {
+                st.push('0');
+                st.push(value);
+                currExpr = '0' + value;
+                return;
+            }
+
+            if(isOperation(st.top())) {
+                st.pop();
+                currExpr = currExpr.substring(0, currExpr.length - 1);
+                currExpr += value;
+            }
+
+            st.push(value);
+            currVal = "";
+            displayExpression.value = currExpr;
+            return;
+        }
+        
+        currVal += value;
+        currExpr += value;
+        displayCurrentValue.value = currVal;
+        displayExpression.value = currExpr;
+        if(st.empty() || isOperation(st.top())) {
+            st.push(currVal);
+        } else {
+            let temp = st.top();
+            st.pop();
+
+            st.push(temp + value + value);
+        }
+
+        displayExpression.scrollLeft = displayExpression.scrollWidth;
     });
 });
+
+displayExpression.scrollLeft = displayExpression.scrollWidth;
