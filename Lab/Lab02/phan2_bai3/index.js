c1cefab33aeafe1ea7e50412587accf08b537c5d
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-03-04 21:33:16 +0700

    update phan2_bai3 lab02

--- Lab/Lab02/phan2_bai3/index.js
+++ Lab/Lab02/phan2_bai3/index.js
@@ -15,10 +15,127 @@ for(let i = 2025; i >= 1900; i --) {
 }
 tempDiv.innerHTML = tempContent;
 
+function isValidDate(year, month, day) {
+    var date = new Date(year, month - 1, day);
+    return (
+        date.getFullYear() == year &&
+        date.getMonth() + 1 == month &&
+        date.getDate() == day
+    );
+}
+
+function birthdateCheck() {
+    if(
+        $("#month-birth").val() === "m" || 
+        $("#date-birth").val() === "d" || 
+        $("#year-birth").val() === "y" || 
+        !isValidDate($("#year-birth").val(), $("#month-birth").val(), $("#date-birth").val() === "d")
+    ) {
+        $("#month-birth").addClass("is-invalid");
+        $('label[for="month-birth"]').css({
+            "color": "red"
+        });
+
+        $("#date-birth").addClass("is-invalid");
+        $('label[for="date-birth"]').css({
+            "color": "red"
+        });
+
+        $("#year-birth").addClass("is-invalid");
+        $('label[for="year-birth"]').css({
+            "color": "red"
+        });
+    } else {            
+        $("#month-birth").removeClass("is-invalid");
+        $("#month-birth").addClass("is-valid");
+        $('label[for="month-birth"]').css({
+            "color": "grey"
+        });
+
+        $("#date-birth").removeClass("is-invalid");
+        $("#date-birth").addClass("is-valid");
+        $('label[for="date-birth"]').css({
+            "color": "grey"
+        });
+
+        $("#year-birth").removeClass("is-invalid");
+        $("#year-birth").addClass("is-valid");
+        $('label[for="year-birth"]').css({
+            "color": "grey"
+        });
+    }
+}
+
+function reset() {
+    $("#first-name").val("");
+    $("#last-name").val("");
+    $("#month-birth").val("m");
+    $("#date-birth").val("d");
+    $("#year-birth").val("y");
+    $("#email").val("");
+    $("#password").val("");
+    $("#confirm-password").val("");
+    $("#about").val("");
+
+    $("#month-birth").removeClass("is-invalid");
+    $('label[for="month-birth"]').css({
+        "color": "grey"
+    });
+
+    $("#date-birth").removeClass("is-invalid");
+    $('label[for="date-birth"]').css({
+        "color": "grey"
+    });
+
+    $("#year-birth").removeClass("is-invalid");
+    $('label[for="year-birth"]').css({
+        "color": "grey"
+    });
+
+    $("#first-name").removeClass("is-invalid");
+    $('label[for="first-name"]').css({
+        "color": "grey"
+    });
+
+    $("#last-name").removeClass("is-invalid");
+    $('label[for="last-name"]').css({
+        "color": "grey"
+    });
+
+    $("#email").removeClass("is-invalid");
+    $('label[for="email"]').css({
+        "color": "grey"
+    });
+
+    $("#password").removeClass("is-invalid");
+    $('label[for="password"]').css({
+        "color": "grey"
+    });
+
+    $("#confirm-password").removeClass("is-invalid");
+    $('label[for="confirm-password"]').css({
+        "color": "grey"
+    });
+
+    $("#about").removeClass("is-invalid");
+    $('label[for="about"]').css({
+        "color": "grey"
+    });
+}
+
+function isValidEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+function checkComplete() {
+
+}
+
 $(document).ready(function() {
     $('#male').prop('checked', true);
 
-    $("#first-name").on("blur", function() {        
+    $("#first-name").on("blur", function() {
         if($("#first-name").val() === "") {
             $("#first-name").addClass("is-invalid");
             $('label[for="first-name"]').css({
@@ -26,13 +143,14 @@ $(document).ready(function() {
             });
         } else {
             $("#first-name").removeClass("is-invalid");
+            $("#first-name").addClass("is-valid");
             $('label[for="first-name"]').css({
                 "color": "grey"
             });
         }
     });
 
-    $("#last-name").on("blur", function() {        
+    $("#last-name").on("blur", function() {
         if($("#last-name").val() === "") {
             $("#last-name").addClass("is-invalid");
             $('label[for="last-name"]').css({
@@ -40,55 +158,66 @@ $(document).ready(function() {
             });
         } else {            
             $("#last-name").removeClass("is-invalid");
+            $("#last-name").addClass("is-valid");
             $('label[for="last-name"]').css({
                 "color": "grey"
             });
         }
     });
 
-    $("#email").on("blur", function() {        
-        if($("#email").val() === "") {
+    $("#month-birth").on("blur", birthdateCheck);
+    $("#date-birth").on("blur", birthdateCheck);
+    $("#year-birth").on("blur", birthdateCheck);
+
+    $("#email").on("blur", function() {
+        if(!isValidEmail($("#email").val())) {
             $("#email").addClass("is-invalid");
             $('label[for="email"]').css({
                 "color": "red"
             });
         } else {            
             $("#email").removeClass("is-invalid");
+            $("#email").addClass("is-valid");
             $('label[for="email"]').css({
                 "color": "grey"
             });
         }
     });
 
-    $("#password").on("blur", function() {        
-        if($("#password").val() === "") {
+    $("#password").on("blur", function() {
+        if($("#password").val().length < 8) {
             $("#password").addClass("is-invalid");
             $('label[for="password"]').css({
                 "color": "red"
             });
         } else {            
             $("#password").removeClass("is-invalid");
+            $("#password").addClass("is-valid");
             $('label[for="password"]').css({
                 "color": "grey"
             });
         }
     });
 
-    $("#confirm-password").on("blur", function() {        
-        if($("#confirm-password").val() === "") {
+    $("#confirm-password").on("blur", function() {
+        if($("#confirm-password").val().length < 8) {
             $("#confirm-password").addClass("is-invalid");
             $('label[for="confirm-password"]').css({
                 "color": "red"
             });
+        } if($("#confirm-password").val() != $("#password").val()) {
+            $("#confirm-password").addClass("is-invalid");
+            $('#invalid-confirm-password span').text('Not same with above password.');
         } else {            
             $("#confirm-password").removeClass("is-invalid");
+            $("#confirm-password").addClass("is-valid");
             $('label[for="confirm-password"]').css({
                 "color": "grey"
             });
         }
     });
 
-    $("#about").on("blur", function() {        
+    $("#about").on("blur", function() {
         if($("#about").val() === "") {
             $("#about").addClass("is-invalid");
             $('label[for="about"]').css({
@@ -96,9 +225,13 @@ $(document).ready(function() {
             });
         } else {            
             $("#about").removeClass("is-invalid");
+            $("#about").addClass("is-valid");
             $('label[for="about"]').css({
                 "color": "grey"
             });
         }
     });
+
+    $("#reset-button").click(reset);
+    $("#submit-button").click(checkComplete);
 });
