d43ae0a343a1c61b845501e7ae566d74ba5c419f
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-03-20 11:44:55 +0700

    update phan2_bai2 lab03

--- Lab/Lab03/phan2_bai2/connect.php
+++ Lab/Lab03/phan2_bai2/connect.php
@@ -2,5 +2,11 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
+    $dbname = "shop";
 
-    $conn = new PDO("mysql:host=$servername;dbname=lab03", $username, $password);
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
+    } catch(PDOException $e) {
+        echo "<script>alert(\"Connection failed: " . $e->getMessage() . ")</script>";
+    }
--- Lab/Lab03/phan2_bai2/list.php
+++ Lab/Lab03/phan2_bai2/list.php
@@ -1,3 +1,4 @@
+<?php include("connect.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="list_style.css">
-    <title>Danh sách sản phẩm</title>
+    <title>Danh Sách Sản Phẩm</title>
 </head>
 <body>    
     <header>
@@ -53,18 +54,27 @@
 
     <main class="container" style="margin-top: 4rem;">
         <div class="row">
-            <!-- <div class="col-3">
-                <div class="card">
-                    <img src="https://m.media-amazon.com/images/I/31UpGSJdmqL._AC_.jpg" class="card-img-top" alt="...">
-                    
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <a href="#" class="btn btn-primary">Mua Ngay</a>
-                    </div>
-                </div>
-            </div> -->
-        
             <?php
+                $statment = $conn->prepare("SELECT id, price, name, image FROM products");
+                $statment->execute();
+                $result = $statment->fetchAll(PDO::FETCH_ASSOC);
+                
+                foreach($result as $row) {
+                    $price = number_format($row['price'], 0, ',', '.') . ' đ';
+                    $currentPage = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+                    $directory = dirname($currentPage);
+
+                    print "<div class=\"col-3\">
+                        <div class=\"card\">
+                            <img src=\"{$row['image']}\" class=\"card-img-top\" alt=\"...\">
+                            
+                            <div class=\"card-body\">
+                                <h6 class=\"card-title\">{$row['name']}</h6>
+                                <a href=\"$directory/detail.php?id={$row['id']}\" class=\"btn btn-primary\">{$price}</a>
+                            </div>
+                        </div>
+                    </div>";
+                }
             ?>
         </div>
     </main>
