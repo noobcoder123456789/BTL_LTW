d5dfc6dcbce94324d4f3686b21c8b71ecc15070a
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-03-20 07:07:58 +0700

    update phan2_bai1 lab03

--- Lab/Lab03/phan1_bai6/index.php
+++ Lab/Lab03/phan1_bai6/index.php
@@ -7,7 +7,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="style.css">
-    <title>Sign Up</title>
+    <title>Phần 1 Bài 6</title>
 </head>
 <body>
     <form method="POST">
