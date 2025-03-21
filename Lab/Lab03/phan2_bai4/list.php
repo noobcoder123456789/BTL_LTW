0abd9486ed3758c3cbf20201b8c69aecb20a50a3
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-03-21 21:09:24 +0700

    update phan2_bai4 lab03

--- Lab/Lab03/phan2_bai4/list.php
+++ Lab/Lab03/phan2_bai4/list.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="list_style.css">
     <title>Danh Sách Sản Phẩm</title>
 </head>
@@ -54,13 +55,24 @@
 
     <main class="container" style="margin-top: 4rem;">
         <div class="row">
+            <!-- <div class="col-3">
+                <div class="card">
+                    <img src="https://m.media-amazon.com/images/I/31UpGSJdmqL._AC_.jpg" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                        <h6 class="card-title">Usb Kingston 3.0</h6>
+                        <a href="" class="btn btn-primary">42000</a>
+                    </div>
+                </div>
+            </div> -->
+
             <?php
                 $statment = $conn->prepare("SELECT id, price, name, image FROM products");
                 $statment->execute();
                 $result = $statment->fetchAll(PDO::FETCH_ASSOC);
                 
                 foreach($result as $row) {
-                    $price = number_format($row['price'], 0, ',', '.') . ' đ';
+                    $price = number_format($row['price'], 0, ',', '.') . 'đ';
                     $currentPage = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
                     $directory = dirname($currentPage);
 
@@ -70,13 +82,15 @@
                             
                             <div class=\"card-body\">
                                 <h6 class=\"card-title\">{$row['name']}</h6>
-                                <a href=\"$directory/detail.php?id={$row['id']}\" class=\"btn btn-primary\">{$price}</a>
+                                <a href=\"?id={$row['id']}\" class=\"btn btn-primary\">{$price}</a>
                             </div>
                         </div>
                     </div>";
                 }
             ?>
         </div>
+
+        <div class="row detail"></div>
     </main>
 
     <footer>
@@ -115,5 +129,21 @@
             </div>
         </div>
     </footer>
+
+    <script>
+        $(document).ready(function() {
+            $(".card-body a.btn.btn-primary").click(function(e) {
+                e.preventDefault();
+                $.ajax({
+                    url: "detail.php",
+                    type: "GET",
+                    data: {id: $(this).attr('href').split('=')[1]},
+                    success: function(response) {
+                        $(".detail").html(response);
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 </html>
