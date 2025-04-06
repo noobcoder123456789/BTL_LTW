6cae8107da7e2ee88d3f3650d12d0a50a4fbf96b
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-04-06 15:56:27 +0700

    update phan2_bai2 lab04

--- Lab/Lab04/phan2_bai2/server.php
+++ Lab/Lab04/phan2_bai2/server.php
@@ -19,6 +19,7 @@
             $stmt = $conn->prepare($sql);
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            http_response_code(200);
             echo json_encode($result);
         } elseif($_SERVER['REQUEST_METHOD'] === 'POST') {
             $data = json_decode(file_get_contents("php://input"));
